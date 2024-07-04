58b4a478b3abe883ab51d42cd14b199f952c3f73
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-04 01:20:28 -0500

    created post route

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,19 @@ export type Env = {
 
 const app = new Hono<{ Bindings: Env }>()
 
-app.get('/posts', async (c) => {
+app
+.get('/posts', async (c) => {
   const db = drizzle(c.env.DB)
   const result = await db.select().from(posts).all()
+  return c.json(result)
+})
+.post('/posts', async (c) => {
+  const db=drizzle(c.env.DB)
+  const { title, content } = await c.req.json()
+  const result = await db 
+    .insert(posts)
+    .values({title, content})
+    .returning()
   return c.json(result)
 })
 
